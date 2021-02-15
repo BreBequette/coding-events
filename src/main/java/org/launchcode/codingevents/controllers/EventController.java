13f1c2bb37cb18c1acfb0fec425faf2ee2e8df18
Bre Bequette Reuther <67883123+BreBequette@users.noreply.github.com>
2021-02-15 15:58:20 -0600

    finished exercise

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -8,16 +8,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 @Controller
 @RequestMapping("events")
 public class EventController {
 
-    private static List<String> events = new ArrayList<>();
+    private static HashMap<String, String> events = new HashMap<>();
 
     @GetMapping
     public String displayAllEvents(Model model){
+
+        events.put("LC101 Graduation", "Class of March 2021 LaunchCode Graduation");
+        events.put("Code Camp", "Learn to code in the great outdoors");
+        events.put("STL Hackathon", "Join STL-area coders to work on anything you're interested in");
         model.addAttribute("events", events);
         return "events/index";
     }
@@ -30,8 +35,8 @@ public class EventController {
 
     //lives at /events/create - OK because they have 2 diff types of requests
     @PostMapping("create")
-    public String createEvent(@RequestParam String eventName){
-        events.add(eventName);
+    public String createEvent(@RequestParam String eventName, @RequestParam String eventDescription){
+        events.put(eventName,eventDescription);
         return "redirect:";
     }
 
